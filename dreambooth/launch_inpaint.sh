adc1690d9e7fb78870489460322c2ec1365b041a
Putri Azizah Hana Rosa <52850023+pazizahhr@users.noreply.github.com>
2022-11-10 05:47:11 +0700

    Add files via upload

---
+++ dreambooth/launch_inpaint.sh
@@ -0,0 +1,26 @@
+export MODEL_NAME="runwayml/stable-diffusion-inpainting"
+export OUTPUT_DIR="../../../models/dress_inpainting"
+
+accelerate launch train_inpainting_dreambooth.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --pretrained_vae_name_or_path="stabilityai/sd-vae-ft-mse" \
+  --output_dir=$OUTPUT_DIR \
+  --with_prior_preservation --prior_loss_weight=1.0 \
+  --seed=3434554 \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --train_text_encoder \
+  --mixed_precision="fp16" \
+  --use_8bit_adam \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=1e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --num_class_images=50 \
+  --sample_batch_size=4 \
+  --max_train_steps=1200 \
+  --save_interval=400 \
+  --save_sample_prompt="photo of woman wearing ukj colored zwx dress" \
+  --concepts_list="concepts_list.json" \
+  --pad_tokens \
+  --not_cache_latents
